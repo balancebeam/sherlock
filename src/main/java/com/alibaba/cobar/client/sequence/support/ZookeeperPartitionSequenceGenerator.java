8927078646d2aa8d36b5fca99fed4c3fdf5c180f
jinfei21 <63581560@qq.com>
2016-05-13 18:22:25 +0800

    no message

--- src/main/java/com/alibaba/cobar/client/sequence/support/ZookeeperPartitionSequenceGenerator.java
+++ src/main/java/com/alibaba/cobar/client/sequence/support/ZookeeperPartitionSequenceGenerator.java
@@ -24,45 +24,44 @@ import com.alibaba.cobar.client.sequence.SequenceGenerator;
 public class ZookeeperPartitionSequenceGenerator implements SequenceGenerator, InitializingBean {
 
 	private Log logger = LogFactory.getLog(ZookeeperPartitionSequenceGenerator.class);
-		
+
 	private ConcurrentHashMap<String, Future<AtomicLong>> sequenceRepository = new ConcurrentHashMap<>();
 
 	private CuratorFramework client;
 
 	private String zkAddr;
-	
+
 	private volatile long boundaryMaxValue = 0;
-	
+
 	private long incrStep = 1000;
-	
+
 	private int retry = 3;
 
 	public void setZKAddr(String zkAddr) {
 		this.zkAddr = zkAddr;
 	}
-	
 
 	public void setIncrStep(long incrStep) {
 		this.incrStep = incrStep;
 	}
-	
-	public void setRetry(int retry){
+
+	public void setRetry(int retry) {
 		this.retry = retry;
 	}
 
 	@Override
 	public long nextval(String name) {
-		
+
 		Future<AtomicLong> future = sequenceRepository.get(name);
 		AtomicLong nextval = null;
-		
+
 		try {
 			if (future == null) {
 				FutureTask<AtomicLong> newFuture = new FutureTask<>(new DBSequenceBuilderTask(name));
 				if (sequenceRepository.putIfAbsent(name, newFuture) == null) {
 					newFuture.run();
 				}
-				if(null== (future = sequenceRepository.get(name))){
+				if (null == (future = sequenceRepository.get(name))) {
 					return nextval(name);
 				}
 			}
@@ -72,84 +71,93 @@ public class ZookeeperPartitionSequenceGenerator implements SequenceGenerator, I
 			sequenceRepository.remove(name);
 			throw new ShardingException(e);
 		}
-		
+
 		long val = nextval.incrementAndGet();
-		if(val<= boundaryMaxValue){
-			if(val == boundaryMaxValue){
+		if (val <= boundaryMaxValue) {
+			if (val == boundaryMaxValue) {
 				sequenceRepository.remove(name);
 			}
 			return val;
 		}
-		
-		for(;;){
-			if(logger.isDebugEnabled()){
-				logger.debug("sequence ["+name+"] outboundary, val="+val+",boundaryMaxValue="+boundaryMaxValue);
+
+		for (;;) {
+			if (logger.isDebugEnabled()) {
+				logger.debug(
+						"sequence [" + name + "] outboundary, val=" + val + ",boundaryMaxValue=" + boundaryMaxValue);
 			}
-			Future<AtomicLong> future2= sequenceRepository.get(name);
-			if(future2!= future){
+			Future<AtomicLong> future2 = sequenceRepository.get(name);
+			if (future2 != future) {
 				return nextval(name);
 			}
-		}		
+		}
 	}
-	
+
 	class DBSequenceBuilderTask implements Callable<AtomicLong> {
-		
+
 		private String name;
 
 		DBSequenceBuilderTask(String name) {
 			this.name = name;
 		}
-		
-		private AtomicLong getStartIndex() throws Exception{
-			String data = new String(client.getData().forPath("/seq/"+name), Charset.forName("UTF-8"));
+
+		private AtomicLong getStartIndex() throws Exception {
+			String data = new String(client.getData().forPath("/seq/" + name), Charset.forName("UTF-8"));
 			long max = Long.parseLong(data);
-			
+
 			boundaryMaxValue = max + incrStep;
-			
-			client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/seq/"+name, String.valueOf(boundaryMaxValue).getBytes(Charset.forName("UTF-8")));
-			
-			return new AtomicLong(max); 
+
+			client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/seq/" + name,
+					String.valueOf(boundaryMaxValue).getBytes(Charset.forName("UTF-8")));
+
+			return new AtomicLong(max);
 		}
 
 		@Override
 		public AtomicLong call() throws Exception {
 
-			
-			while(true){
-				InterProcessMutex lock = new InterProcessMutex(client, "/lock/"+name);
-				
-				if(null != client.checkExists().forPath("/seq/"+name)){//已经存在
-					lock.acquire();
-					return getStartIndex();
-				}else{//不存在
-					lock.acquire();
-					if(null == client.checkExists().forPath("/seq/"+name)){
-						String data = String.valueOf(incrStep);
-						client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/seq/"+name, data.getBytes(Charset.forName("UTF-8")));
-						boundaryMaxValue= incrStep;
-						return new AtomicLong(0);
-					}else{
+			while (true) {
+				InterProcessMutex lock = new InterProcessMutex(client, "/lock/" + name);
+				try {
+					if (null != client.checkExists().forPath("/seq/" + name)) {// 已经存在
+						lock.acquire();
 						return getStartIndex();
+					} else {// 不存在
+						lock.acquire();
+						if (null == client.checkExists().forPath("/seq/" + name)) {
+							String data = String.valueOf(incrStep);
+							client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+									.forPath("/seq/" + name, data.getBytes(Charset.forName("UTF-8")));
+							boundaryMaxValue = incrStep;
+							return new AtomicLong(0);
+						} else {
+							return getStartIndex();
+						}
 					}
+				} catch(Exception e){
+					
+					if(retry-- == 0){
+						throw e;
+					}
+				}finally {
+					lock.release();
 				}
-				
+
 			}
 
 		}
-		
+
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 
-		Builder builder = CuratorFrameworkFactory.builder()
-												.connectString(zkAddr)
-												.retryPolicy(new ExponentialBackoffRetry(1000, 3, 3000)).namespace("cobarclientx");
+		Builder builder = CuratorFrameworkFactory.builder().connectString(zkAddr)
+				.retryPolicy(new ExponentialBackoffRetry(1000, 3, 3000)).namespace("cobarclientx");
 		client = builder.build();
 		client.start();
-		try{
+		try {
 			client.blockUntilConnected();
-		}catch(Exception e){
+		} catch (Exception e) {
 			logger.error("", e);
 			throw e;
 		}
