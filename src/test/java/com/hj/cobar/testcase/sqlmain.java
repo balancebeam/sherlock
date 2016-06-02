f42aef1fdb2088d9e2531bc26bf18b24ba993de8
jinfei21 <63581560@qq.com>
2016-06-02 14:13:27 +0800

    fix delete bug

--- src/test/java/com/hj/cobar/testcase/sqlmain.java
+++ src/test/java/com/hj/cobar/testcase/sqlmain.java
@@ -6,22 +6,26 @@ import com.alibaba.cobar.client.sqlparser.SQLParseEngine;
 import com.alibaba.cobar.client.sqlparser.SQLParsedResult;
 import com.alibaba.cobar.client.sqlparser.SQLParserFactory;
 import com.alibaba.cobar.client.sqlparser.bean.Condition;
-import com.alibaba.cobar.client.sqlparser.bean.DatabaseType;
 import com.alibaba.cobar.client.sqlparser.bean.Table;
 import com.google.common.collect.Lists;
+
+import io.pddl.datasource.DatabaseType;
 
 public class sqlmain {
 
 	public static void main(String[] args) {
 
 
-		 String sql ="SELECT * FROM test1.cont a where a.id=? or bbb=? and a.name=? or a.sex=nam"; 
+		 //String sql ="SELECT * FROM test1.cont a where a.id=? or bbb=? and a.name=? or a.sex=nam"; 
+		 String sql ="DELETE FROM t_item_ext WHERE item_id IN (?, ?, ?)";
+		 
 		 List<Object> list = Lists.newArrayList();
 		 list.add(777);
 		 list.add(9);
 		 list.add(2);
+		 DatabaseType.setApplicationDatabaseType(DatabaseType.PostgreSQL);
 		 
-		 SQLParseEngine engine = SQLParserFactory.create(DatabaseType.POSTGRESQL, sql, list);
+		 SQLParseEngine engine = SQLParserFactory.create(sql, list);
 		 SQLParsedResult result = engine.parse();
 		 
 		 System.out.println(result.getSqlBuilder().toString());
