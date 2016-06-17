b87715cbcf8b07dab4c26c2bbf99ad9961ed7f71
artoderk <artoderk@google.com>
2016-06-17 14:14:24 +0800

    revert

--- src/main/java/io/pddl/datasource/support/strategy/WeightReadStrategySupport.java
+++ src/main/java/io/pddl/datasource/support/strategy/WeightReadStrategySupport.java
@@ -15,16 +15,14 @@ import io.pddl.datasource.DataSourceReadStrategy;
 import io.pddl.datasource.support.PartitionDataSourceSupport;
 
 public class WeightReadStrategySupport implements DataSourceReadStrategy{
-	
+
 	private Log logger = LogFactory.getLog(getClass());
 
-	private int totalWeight = 0;
-	
 	@Override
 	public DataSource getSlaveDataSource(PartitionDataSource pds) {
 		return getDataSourceByWeight(pds,0);
 	}
-	
+
 	protected DataSource getDataSourceByWeight(PartitionDataSource pds,int w){
 		List<DataSource> slaveDataSources= ((PartitionDataSourceSupport)pds).getSlaveDataSources();
 		if(CollectionUtils.isEmpty(slaveDataSources)){
@@ -33,14 +31,13 @@ public class WeightReadStrategySupport implements DataSourceReadStrategy{
 			}
 			return pds.getMasterDataSource();
 		}
-
-		if (totalWeight == 0) {
-			// cache total weight
-			calculateTotalWeight(slaveDataSources);
+		int total= 0;
+		//TODO this need cache
+		for(DataSource ds: slaveDataSources){
+			total+= ((WeightDataSourceProxy)ds).getWeight();
 		}
-
 		Random rand = new Random();
-		int weight= 0,rdm= rand.nextInt(totalWeight + w);
+		int weight= 0,rdm= rand.nextInt(total+ w);
 		for(DataSource ds: slaveDataSources){
 			weight+= ((WeightDataSourceProxy)ds).getWeight();
 			if(weight> rdm){
@@ -51,21 +48,11 @@ public class WeightReadStrategySupport implements DataSourceReadStrategy{
 			}
 		}
 		return pds.getMasterDataSource();
-    }
-	
+	}
+
 	@Override
 	public String getStrategyName(){
 		return "weight";
-	}
-
-	private synchronized void calculateTotalWeight(List<DataSource> lds) {
-		if (totalWeight != 0) return;
-
-		int total = 0;
-		for(DataSource ds: lds){
-			total+= ((WeightDataSourceProxy)ds).getWeight();
-		}
-		totalWeight = total;
 	}
 
 }
