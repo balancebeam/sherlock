50f990f4d0b1b5b894d19e5fb2346f0207df6728
artoderk <artoderk@google.com>
2016-06-23 16:41:31 +0800

    add group by merge

--- pddl-core/src/main/java/io/pddl/merger/pipeline/coupling/GroupByCouplingResultSet.java
+++ pddl-core/src/main/java/io/pddl/merger/pipeline/coupling/GroupByCouplingResultSet.java
@@ -16,17 +16,16 @@ import java.util.List;
 /**
  * 分组的连接结果集.
  * 
- * @author gaohongtao
- * @author zhangliang
+ * @author xiong.jie
  */
 public final class GroupByCouplingResultSet extends AbstractMemoryResultSet {
-    
+
     private final List<GroupColumn> groupByColumns;
     
     private final List<AggregationColumn> aggregationColumns;
     
     private ResultSet resultSet;
-    
+
     private boolean hasNext;
     
     public GroupByCouplingResultSet(final ResultSet resultSet, final MergeContext mc) throws SQLException {
@@ -34,18 +33,19 @@ public final class GroupByCouplingResultSet extends AbstractMemoryResultSet {
         groupByColumns = mc.getGroupColumns();
         aggregationColumns = mc.getAgregationColumns();
     }
-    
+
     @Override
     protected void initRows(final List<ResultSet> resultSets) throws SQLException {
         resultSet = resultSets.get(0);
         hasNext = resultSet.next();
     }
-    
+
     @Override
     protected Optional<? extends ResultSetRow> nextRow() throws SQLException {
         if (!hasNext) {
             return Optional.absent();
         }
+
         GroupByResultSetRow result = new GroupByResultSetRow(resultSet, groupByColumns, aggregationColumns);
         List<Object> groupByValues = result.getGroupByValues();
         while (hasNext && (groupByColumns.isEmpty() || groupByValues.equals(result.getGroupByValues()))) {
